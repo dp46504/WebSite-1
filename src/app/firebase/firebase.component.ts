4f67c9ee1990f711216330f5025d2fdbbd80d009
Paweł Durczak <kissonion24@gmail.com>
2024-02-26 22:22:27 +0100

    Stopped tracking .env File

---
+++ src/app/firebase/firebase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firebase',
+  standalone: true,
+  imports: [],
+  templateUrl: './firebase.component.html',
+  styleUrl: './firebase.component.sass'
+})
+export class FirebaseComponent {
+
+}
