80fe499a117af3a4ae545cf8b4ccd8a4820a7c7e
Paweł Durczak <kissonion24@gmail.com>
2024-02-26 23:08:45 +0100

    Skeleton added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {CategoryComponent} from "./category/category.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:"", component: HomeComponent, pathMatch:"full" },
+  { path: "category", component: CategoryComponent },
+  { path:"**", component: NotFoundComponent }
+];
